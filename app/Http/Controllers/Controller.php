d886cd01c36d60113476b186391ef236dbda6927
fredriexPramana <22410100002@dinamika.ac.id>
2024-12-20 14:45:54 +0700

    2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
-abstract class Controller
-{
-    //
+class Controller extends BaseController{
+    
+    public function home(){
+        return view('home');
+    }
+
 }
